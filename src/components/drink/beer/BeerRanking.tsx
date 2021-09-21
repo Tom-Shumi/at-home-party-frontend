7bb365e425af34498101e80c8d18117d2a4ea2c6
Tom-Shumi <tshumiya281@gmail.com>
2021-09-21 23:14:16 +0900

    Add Beer page WIP   Beer Ranking WIP

--- src/components/drink/beer/BeerRanking.tsx
+++ src/components/drink/beer/BeerRanking.tsx
@@ -1,14 +1,53 @@
+import {Table} from 'react-bootstrap';
+import styles from '/styles/drink/beer/BeerRanking.module.css';
+import { Drink } from 'types/Drink';
+import Image from 'next/image';
+import ReactStars from 'react-stars';
+import {Constant} from 'components/Constant';
+
 
 interface BeerRankingProps {
 }
 
 const BeerRanking: React.FC<BeerRankingProps> = (props) => {
 
-
+  const headerColspan = 2;
 
   return (
     <>
-      BeerRanking
+      <Table hover>
+      <thead>
+        <tr className="centerTr">
+          <th className="numberTh">#</th>
+          <th colSpan={headerColspan} >商品</th>
+          <th className="starTh">星</th>
+          <th className="detailTh">苦味</th>
+          <th className="detailTh">香り</th>
+          <th className="detailTh">ホップ</th>
+          <th className="detailTh">キレ</th>
+          <th className="detailTh">ボディ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="rankingTr">
+          <td className="centerTd">1</td>
+          <td className="imageTd"><Image src="https://images-fe.ssl-images-amazon.com/images/P/B01BM9ECRE.09.TZZZZZZZ" alt="beer" width={70} height={70} /></td>
+          <td>ビール1位</td>
+          <td className="centerTd"><ReactStars
+                  count={5}
+                  value={3}
+                  size={24}
+                  color1={'#d3d3d3'}
+                  color2={'#ffd700'}
+                  edit={false} /></td>
+          <td className="centerTd">5</td>
+          <td className="centerTd">5</td>
+          <td className="centerTd">5</td>
+          <td className="centerTd">5</td>
+          <td className="centerTd">5</td>
+        </tr>
+      </tbody>
+    </Table>
     </>
   )
 }
--- src/components/home/DrinkRankingTable.tsx
+++ src/components/home/DrinkRankingTable.tsx
@@ -17,10 +17,10 @@ const DrinkRankingTable: React.FC<DrinkRankingTableProps> = (props) => {
   return (
     <Table hover>
       <thead>
-        <tr>
-          <th className={styles.numberTh}>#</th>
+        <tr className="centerTr">
+          <th className="numberTh">#</th>
           <th colSpan={headerColspan} >商品</th>
-          <th>星</th>
+          <th className="starTh">星</th>
           <th>説明</th>
         </tr>
       </thead>
@@ -29,16 +29,16 @@ const DrinkRankingTable: React.FC<DrinkRankingTableProps> = (props) => {
           let color = "rankingColor" + (count + 1);
 
           return (
-            <tr key={props.drinkTypeName + "Ranking" + (count + 1)} className={styles.rankingTr + Constant.CSS_JOIN + color} >
-              <td className={styles.numberTd}>{count + 1}</td>
-              <td className={styles.imageTd}>
+            <tr key={props.drinkTypeName + "Ranking" + (count + 1)} className={"rankingTr" + Constant.CSS_JOIN + color} >
+              <td className="centerTd">{count + 1}</td>
+              <td className="imageTd">
                 {drink.infoUrl != null &&
                   <Image key={props.drinkTypeName + "RankingImage" + (count + 1)}
                   src={drink.infoUrl} alt={props.drinkTypeName + "RankingImage" + (count + 1)} width={70} height={70} />
                 }
               </td>
               <td className={styles.nameTd}>{drink.name}</td>
-              <td className={styles.starTd}>
+              <td>
                 <ReactStars
                   count={5}
                   value={drink.star}
