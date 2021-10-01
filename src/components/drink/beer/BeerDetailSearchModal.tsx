b6e7919ae083b4d1dc0cee2f0d6620d27fd05c62
Tom-Shumi <tshumiya281@gmail.com>
2021-10-01 22:42:32 +0900

    add beer detail search modal layout DONE

--- src/components/drink/beer/BeerDetailSearchModal.tsx
+++ src/components/drink/beer/BeerDetailSearchModal.tsx
@@ -10,16 +10,64 @@ interface BeerDetailSearchModalProps {
 const BeerDetailSearchModal: React.FC<BeerDetailSearchModalProps> = (props) => {
 
   return (
-    <Modal show={true} onHide={props.close} key='beerDetailSearchModal' animation = {false}>
+    <Modal show={true} onHide={props.close} key='beerDetailSearchModal' animation = {false} className="modal">
       <Modal.Header>
-        <Modal.Title>詳細検索</Modal.Title>
+        <Modal.Title className="modalTitle">詳細検索</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Row>
-          <Col>商品名</Col>
-          <Col></Col>
+          <Col xs={3} className="modalLabel">商品名</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalFullText" />
+          </Col>
+          <Col xs={3} className="modalLabel">星</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
+          <Col xs={3} className="modalLabel">度数</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> ％
+            　〜　
+            <input type="text" className="modalRangeText" /> ％
+          </Col>
+          <Col xs={3} className="modalLabel">苦味</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
+          <Col xs={3} className="modalLabel">香り</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
+          <Col xs={3} className="modalLabel">ホップ</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
+          <Col xs={3} className="modalLabel">キレ</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
+          <Col xs={3} className="modalLabel">ボディ</Col>
+          <Col xs={9} className="modalInput">
+            <input type="text" className="modalRangeText" /> 点
+            　〜　
+            <input type="text" className="modalRangeText" /> 点
+          </Col>
         </Row>
       </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-warning" className="modalButton">詳細検索</Button>
+        <Button variant="outline-dark" className="modalButton">閉じる</Button>
+      </Modal.Footer>
     </Modal>
   )
 }
